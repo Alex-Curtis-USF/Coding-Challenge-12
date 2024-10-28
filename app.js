e9ced2f2ad74648f899d98401c4e20ad468968c1
Alexander Curtis <ajcurtis@usf.edu>
2024-10-27 21:55:02 -0400

    Implement shape drawing logic for lines, rectangles, and circles.

--- app.js
+++ app.js
@@ -20,9 +20,44 @@ function startDrawing(e) {
 
 function draw(e) {
     if (!isDrawing) return;
+    // Task 3
+    // This is to have the drawing logic.
+    const tool = document.querySelector('input[name="tool"]:checked').value;
     
+    // To clear the canvas.
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // This is where the drawing logic with shapes begin
+    ctx.beginPath();
+    switch (tool) {
+        case 'line':
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(e.offsetX, e.offsetY);
+            break;
+            
+        case 'rectangle':
+            const width = e.offsetX - startX;
+            const height = e.offsetY - startY;
+            ctx.rect(startX, startY, width, height);
+            break;
+            
+        case 'circle':
+            const radius = Math.sqrt(
+                (e.offsetX - startX) ** 2 + 
+                (e.offsetY - startY) ** 2
+            );
+            ctx.arc(startX, startY, radius, 0, Math.PI * 2);
+            break;
+    }
+    ctx.stroke();
 }
 
 function stopDrawing() {
     isDrawing = false;
 }
+
+// Actions in order to create shapes.
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
